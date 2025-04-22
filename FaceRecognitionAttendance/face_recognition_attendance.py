9098c7ce2c4fd0e5d72a46a94d5a82d9f525551c
StudentGenious <studentgenious110@gmail.com>
2025-04-22 13:11:39 +0500

    Create face_recognition_attendance.py

---
+++ FaceRecognitionAttendance/face_recognition_attendance.py
@@ -0,0 +1,4 @@
+"""
+UPCOMING PROJECT
+this is going to be the project that will help education system to provide the facility o mark attandance automatically in the basses of detecting the student'face
+"""
